705e9c68b01f921a59eaf3147ee8781114ec310f
rausqen11 <ts1460omerkoc34@gmail.com>
2026-01-15 00:50:07 +0300

    Move Tailwind to build and remove CDN

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './**/*.{ts,tsx,js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Playfair Display', 'serif'],
+        serif: ['Playfair Display', 'serif'],
+        ui: ['IBM Plex Sans', 'sans-serif'],
+        'cy-grotesk-grand': ['"cy grotesk grand"', 'sans-serif'],
+        'tenor-sans': ['"Tenor Sans"', 'sans-serif'],
+        editorial: ['Bodoni Moda', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
